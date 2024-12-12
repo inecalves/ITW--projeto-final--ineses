4ebdf21e7afd937cc4d2311653c72ef182705276
inecalves <inesgoncalves567@gmail.com>
2024-12-12 23:46:08 +0000

    Algumas mudanças, e atletismo a ser feito

---
+++ Athletics.js
@@ -0,0 +1,118 @@
+// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/api/Athletics/Events');
+    self.displayName = 'Paris2024 Athletics Events List';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    self.events = ko.observableArray([]);
+
+    //---Favoritos -- ainda ver
+    //self.toggleFavourite = function (id) {
+        //if (self.favourites.indexOf(id) == -1) {
+            //self.favourites.push(id);
+        //}
+        //else {
+            //self.favourites.remove(id);
+        //}
+        //localStorage.setItem("fav", JSON.stringify(self.favourites()));
+    //};
+    //self.SetFavourites = function () {
+        //let storage;
+        //try {
+            //storage = JSON.parse(localStorage.getItem("fav"));
+        //}
+        //catch (e) {
+            //;
+        //}
+        //if (Array.isArray(storage)) {
+            //self.favourites(storage);
+        //}
+    //}
+    //self.favourites = ko.observableArray([])
+
+
+    //--- Page Events
+    self.activate = function (id) {
+        console.log('CALL: getEvents...');
+        var composedUri = self.baseUri()
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            console.log(data);
+            hideLoading();
+            self.events(data);
+            //self.SetFavourites();
+        });
+    };
+
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+
+    function sleep(milliseconds) {
+        const start = Date.now();
+        while (Date.now() - start < milliseconds);
+    }
+
+    function showLoading() {
+        $("#myModal").modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+        console.log("sPageURL=", sPageURL);
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('page');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
